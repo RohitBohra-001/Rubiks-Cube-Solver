01d6c5c94a8a83052c43a68ffa43a78c69df0753
RohitBohra-001 <bohrarohit03@gmail.com>
2025-07-04 12:16:02 +0530

    initial msg

---
+++ build/CMakeFiles/rubiks_cube_solver.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube.cpp.o"
+  "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube.cpp.o.d"
+  "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube1dArray.cpp.o"
+  "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube1dArray.cpp.o.d"
+  "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube3dArray.cpp.o"
+  "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCube3dArray.cpp.o.d"
+  "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCubeBitboard.cpp.o"
+  "CMakeFiles/rubiks_cube_solver.dir/Model/RubiksCubeBitboard.cpp.o.d"
+  "CMakeFiles/rubiks_cube_solver.dir/main.cpp.o"
+  "CMakeFiles/rubiks_cube_solver.dir/main.cpp.o.d"
+  "rubiks_cube_solver"
+  "rubiks_cube_solver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rubiks_cube_solver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
